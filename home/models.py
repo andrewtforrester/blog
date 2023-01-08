dc542abbbdd6096dedd161245738b08346b813b5
Andrew Forrester <atf@fastmail.com>
2023-01-07 20:19:49 -0500

    first commit

---
+++ home/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class HomePage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['children'] = BlogEntry.objects.child_of(self).live().order_by('-date')
+        return context
+
+    subpage_types = [
+        'home.BlogEntry'
+    ]
+
+class BlogEntry(Page):
+    date = models.DateField("Post date", null=True, blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        FieldPanel('date'),
+    ]
+
+    parent_page_type = [
+        'home.HomePage'  
+    ]
+
+    subpage_types = [
+    ]
